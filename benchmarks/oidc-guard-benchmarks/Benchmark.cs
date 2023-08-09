9b9e38aa2ebb0c4be35495b8f1c5c9684b764422
Ivan Josipovic <9521987+IvanJosipovic@users.noreply.github.com>
2023-08-08 21:00:42 -0700

    chore: add benchmarks

--- benchmarks/oidc-guard-benchmarks/Benchmark.cs
+++ benchmarks/oidc-guard-benchmarks/Benchmark.cs
@@ -11,6 +11,8 @@ public class Benchmark
 {
     private HttpClient client;
 
+    private string query = "/auth";
+
     [GlobalSetup]
     public void GlobalSetup()
     {
@@ -21,7 +23,8 @@ public class Benchmark
 
         var claims = new List<Claim>()
         {
-            new Claim("tid", "11111111-1111-1111-1111-111111111111")
+            new Claim("tid", "11111111-1111-1111-1111-111111111111"),
+            new Claim("gcip", "{\"auth_time\":1553219869,\"email\":\"demo_user@gmail.com\",\"email_verified\":true,\"firebase\":{\"identities\":{\"email\":[\"demo_user@gmail.com\"],\"saml.myProvider\":[\"demo_user@gmail.com\"]},\"sign_in_attributes\":{\"firstname\":\"John\",\"group\":\"test group\",\"role\":\"admin\",\"lastname\":\"Doe\"},\"sign_in_provider\":\"saml.myProvider\",\"tenant\":\"my_tenant_id\"},\"sub\":\"gZG0yELPypZElTmAT9I55prjHg63\"}")
         };
 
         client.DefaultRequestHeaders.TryAddWithoutValidation(HeaderNames.Authorization, FakeJwtIssuer.GenerateBearerJwtToken(claims));
@@ -30,6 +33,24 @@ public class Benchmark
     [Benchmark]
     public async Task Auth()
     {
-        var response = await client.GetAsync($"/auth");
+        await client.GetAsync(query);
+    }
+
+    [Benchmark]
+    public async Task AuthCheckClaim()
+    {
+        await client.GetAsync(query + "?tid=11111111-1111-1111-1111-111111111111");
+    }
+
+    [Benchmark]
+    public async Task InjectClaims()
+    {
+        await client.GetAsync(query + "?inject-claim=tid");
+    }
+
+    [Benchmark]
+    public async Task InjectJsonClaims()
+    {
+        await client.GetAsync(query + "?inject-json-claim=role,gcip,$.firebase.sign_in_attributes.role");
     }
 }
