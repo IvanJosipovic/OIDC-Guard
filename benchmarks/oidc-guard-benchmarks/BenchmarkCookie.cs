cb37754b6a0b575fbf4b4766146808bb4b0a45f7
Ivan Josipovic <9521987+IvanJosipovic@users.noreply.github.com>
2024-09-28 17:57:46 -0700

    chore: aot bench

--- benchmarks/oidc-guard-benchmarks/BenchmarkCookie.cs
+++ benchmarks/oidc-guard-benchmarks/BenchmarkCookie.cs
@@ -1,4 +1,5 @@
 ﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
 using FluentAssertions;
 using Microsoft.Extensions.Logging;
 using oidc_guard_tests.Infra;
@@ -6,7 +7,9 @@ using System.Net;
 
 namespace oidc_guard_benchmarks;
 
-[MemoryDiagnoser]
+[MemoryDiagnoser()]
+[SimpleJob(RuntimeMoniker.Net90)]
+[SimpleJob(RuntimeMoniker.NativeAot90)]
 public class BenchmarkCookie
 {
     private HttpClient client = default!;
